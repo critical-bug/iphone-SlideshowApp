610c74b2a9325b17773d26cebc610a2361edf1e9
critical-bug <critical-bug@users.noreply.github.com>
2017-01-09 09:28:46 +0000

    (WIP)

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -20,6 +20,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    let thumbImages = ["de488dc5-s.gif",
+        "19cde0ad-s.gif",
+        "20be2a48-s.gif",
+        "36fa3c25-s.gif",
+        "01b6cb78-s.gif",
+        "7ff4dbe2-s.gif",
+        "ef60f98b-s.gif",
+        "c7366e5b-s.gif",
+        "fda6210c-s.gif",
+        "f0134f93-s.gif"]
 
+    var timer: Timer!
+
+    @IBAction func startStopSlideshow(_ sender: Any) {
+        if timer == nil {
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(showNextImage), userInfo:nil, repeats: true)
+        } else{
+            timer.invalidate()
+            timer = nil
+        }
+    }
+    
+    func showNextImage() {
+        
+    }
 }
 
